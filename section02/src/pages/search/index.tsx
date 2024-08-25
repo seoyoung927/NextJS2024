d476bf7b15e0b750be24d40bfa4d68da6e2a6ef2
seoyoung927 <seoyoung0000@naver.com>
2024-08-25 13:00:39 +0900

    :sparkles: [Feat] 한입 북스 UI 구현하기

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,8 +1,17 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  return <h1>search</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
