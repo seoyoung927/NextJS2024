5dcabab4e7457313460733f056cbf460554f642e
seoyoung927 <seoyoung0000@naver.com>
2024-08-25 12:05:26 +0900

    :sparkles: [Feat] 페이지별 레이아웃 구현

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,3 +1,10 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+
 export default function Page() {
   return <h1>search</h1>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
